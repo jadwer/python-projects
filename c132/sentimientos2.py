f3c3fa00782b3c8e3c16ab7b3883cb23244dc56d
Rodrigo G Ramirez M <contacto@gabinoramirez.com>
2023-10-20 10:44:57 -0600

    rename file

---
+++ c132/sentimientos2.py
@@ -0,0 +1,78 @@
+#importamos pandas
+import pandas as pd
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Leemos el excel y lo guardamos en una variable
+train_data_raw = pd.read_excel("Text-Sentiment-Dataset/text-emotion-training-dataset.xlsx")
+
+#imprimimos los 5 primeros datos de nuestro dataframe
+print(train_data_raw.head(5))
+
+# Leemos los datos crudos, pero separamos los datos en texto y sentimiento
+train_data = pd.DataFrame(train_data_raw["Text_Emotion"].str.split(pat=";", n=1).tolist(), columns = ['Text', 'Emotion'])
+
+#imprimimos los 5 primeros datos de nuestro dataframe
+print(train_data.head())
+
+# Imprimimos las emociones diferentes que se encuentren
+print("Emociones:", train_data["Emotion"].unique())
+
+#creamos un diccionario de etiqueta : valor
+encode_emotions = {"anger": 0, "fear": 1, "joy": 2, "love": 3, "sadness": 4, "surprise": 5}
+
+# reemplazamos los valores de las etiquetas encontradas
+train_data.replace(encode_emotions, inplace=True)
+
+#imprimimos el nuevo Dataframe
+print(train_data.head())
+
+# Creamos las listas desde el Dataframe
+
+training_sentences = []
+training_labels = []
+
+for i in range(len(train_data)) :
+    sentence = train_data.loc[i, "Text"]
+    training_sentences.append(sentence)
+
+    label = train_data.loc[i, "Emotion"]
+    training_labels.append(label)
+
+print("Sentence: ", training_sentences[50], "Label:", training_labels[50])
+
+
+# tokenización
+vocab_size = 10000
+embedding_dim = 16
+oov_tok = "<OOV>"
+training_size = 20000
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token = oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+# Creamos un ínice de palabras (diccionario)
+word_index = tokenizer.word_index
+
+print(word_index["the"])
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+
+print(training_sentences[0], training_sequences[0])
+print(training_sentences[1], training_sequences[1])
+print(training_sentences[2], training_sequences[2])
+
+# Normalización de datos
+
+padding_type = 'post'
+max_length = 100
+trunc_type = 'post'
+
+training_padded = pad_sequences(training_sequences, maxlen = max_length, padding=padding_type, truncating = trunc_type)
+
+print(training_padded[0])
+print(training_padded[1])
+print(training_padded[2])
+print(training_padded[3])
