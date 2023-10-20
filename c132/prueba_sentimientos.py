9abed50973e692c67f6a9a04bfec9b1cac381c5c
Rodrigo G Ramirez M <contacto@gabinoramirez.com>
2023-10-20 17:41:36 -0600

    Sentimientos 2

--- c132/prueba_sentimientos.py
+++ c132/prueba_sentimientos.py
@@ -7,6 +7,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 sentence = [
     "I am happy to meet my friends. We are planning to go a party.",
     "I had a bad day at school. i got hurt while playing football",
+#    "I had a bad fight with my friend. I think I have even lost sleep",
+#    "I started the day confused. My student's father sent me a message"
 ]
 
 tokenizer = Tokenizer(num_words = 10000, oov_token = '<OOV>')
