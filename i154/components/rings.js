4834b6355e23da27c5a92a2493cf6122bec614c0
Gabino Ramírez <jadwer@msn.com>
2024-05-28 15:08:16 -0600

    Flying things in airplane project

---
+++ i154/components/rings.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("target-ring", {
+    schema:{},
+    init: function(){
+        for (var i = 1; i <= 20; i++){
+            var id = `ring${i}`
+
+            var posX = (Math.random() * 3000 + (-1000));
+            var posY = (Math.random() * 2 + (-1));
+            var posZ = (Math.random() * 3000 + (-1000));
+
+            var position = {x: posX, y: posY, z: posZ};
+
+            this.createRings(id, position);
+        }
+    },
+
+    createRings: function(id, position){
+        var terrainEl = document.querySelector("#terrain")
+        var ringEl = document.createElement("a-entity");
+
+        ringEl.setAttribute("id", id);
+        ringEl.setAttribute("position", position);
+
+        ringEl.setAttribute("material", "color", "#ff9100");
+        ringEl.setAttribute("geometry", {primitive: "torus", radius: 8});
+        terrainEl.appendChild(ringEl);
+    },
+});
